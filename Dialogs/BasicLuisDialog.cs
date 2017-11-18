36d2e700a63beb09bd44aded58b09b05e9ade8a2
vinstce <util@botframework.com>
2017-11-18 15:47:13 +0000

    prepare to setup source control

---
+++ Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Microsoft.Bot.Sample.LuisBot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        /*public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"], ConfigurationManager.AppSettings["LuisAPIKey"])))
+        {
+        }*/
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute("c4c28e44a22c493faac3c42eb86fb5d4", "f01f819531794fa3858534e2b71bdd4d")))
+        {
+        }
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached the none intent. You said: {result.Query}"); //
+            context.Wait(MessageReceived);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
+        [LuisIntent("AddBirthday")]
+        public async Task MyIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached the MyIntent intent. You said: {result.Query}"); //
+            context.Wait(MessageReceived);
+        }
+    }
+}
